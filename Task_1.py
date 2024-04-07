ebaad85e0daea89f474ae0383565bd36a8cd8acf
AsmarAliyeva7 <164623222+AsmarAliyeva7@users.noreply.github.com>
2024-04-07 20:17:13 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -3,12 +3,11 @@
 def main():
   herf=input("Enter a letter of the alphabet: ")
   if herf=="a" or herf=="e" or herf=="i" or herf=="o" or herf=="u":
-      print("Entered alphabet is a consonant!")
+      print("Entered alphabet is a vowel!")
   elif herf=="y":
       print("Sometimes it is a vowel, and sometimes it is a consonant!")
   else:
-      print("Entered alphabet is a vowel!")
-
+      print("Entered alphabet is a consonant!")
   
   
   
