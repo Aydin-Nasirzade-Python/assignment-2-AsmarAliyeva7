2e366279801cdb546d302014a23ff231fe4e4898
AsmarAliyeva7 <164623222+AsmarAliyeva7@users.noreply.github.com>
2024-04-07 20:33:06 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -1,7 +1,7 @@
 #import libraries here
 
 def main():
-  w=int(input("Enter the wavelengh in nm: "))
+  w=int(input("Enter the wavelength in nm: "))
   if w>=380 and w<450:
       print("The relevant color is Violent")
   elif w>=450 and w<495:
@@ -12,10 +12,11 @@ def main():
       print("The relevant color is Yellow")
   elif w>=590 and w<620:
       print("The relevant color is Orange")
-  elif:
+  elif w>=620 and w<750:
       print("The relevant color is Red")
   else:
       print("Invalid input!")
+  
   
   pass
 
