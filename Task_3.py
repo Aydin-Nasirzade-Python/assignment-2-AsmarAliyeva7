dfe5786060189403eff069ed73553f892b3c98e6
AsmarAliyeva7 <164623222+AsmarAliyeva7@users.noreply.github.com>
2024-04-07 20:28:25 +0400

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -2,21 +2,21 @@
 
 def main():
   w=int(input("Enter the wavelengh in nm: "))
-  if w>=380 and w<=750:
-      if w>=380 and w<450:
-          print("The relevant color is Violent")
-      elif w>=450 and w<495:
-          print("The relevant color is Blue")
-      elif w>=495 and w<570:
-          print("The relevant color is Green")
-      elif w>=570 and w<590:
-          print("The relevant color is Yellow")
-      elif w>=590 and w<620:
-          print("The relevant color is Orange")
-      else:
-          print("The relevant color is Red")
+  if w>=380 and w<450:
+      print("The relevant color is Violent")
+  elif w>=450 and w<495:
+      print("The relevant color is Blue")
+  elif w>=495 and w<570:
+      print("The relevant color is Green")
+  elif w>=570 and w<590:
+      print("The relevant color is Yellow")
+  elif w>=590 and w<620:
+      print("The relevant color is Orange")
+  elif:
+      print("The relevant color is Red")
   else:
       print("Invalid input!")
+  
   pass
 
 if __name__ == "__main__":
