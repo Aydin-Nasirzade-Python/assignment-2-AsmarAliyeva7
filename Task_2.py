d6e9f767aab933f7c41f42257fe236e932bbc2c9
AsmarAliyeva7 <164623222+AsmarAliyeva7@users.noreply.github.com>
2024-04-07 20:21:02 +0400

    Update Task_2.py

--- Task_2.py
+++ Task_2.py
@@ -1,11 +1,11 @@
 #import libraries here
 
 def main():
-  month=input("Enter name of the month [ex. June]: ")
-  day=input("Enter the day [ex. 5]: ")
   a="AprilMay"
   b="JulyAugust"
   c="NovemberOctober"
+  month=input("Enter name of the month [ex. June]: ")
+  day=input("Enter the day [ex. 5]: ")
   if month=="March" and day>=20 or month=="June" and day<21 or month in a:
       print(f"{month} {day} is in Spring")
   elif month=="June" and day>=21 or month=="September" and day<22 or month in b:
@@ -14,7 +14,9 @@ def main():
       print(f"{month} {day} is in Fall")
   else:
       print(f"{month} {day} is in Winter")
-  
+    
+
+    
   pass
 
 if __name__ == "__main__":
