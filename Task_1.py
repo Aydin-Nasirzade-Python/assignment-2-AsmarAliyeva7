220f970ac856da7e6e0e4d2d1f83efcd2f68c7a2
AsmarAliyeva7 <164623222+AsmarAliyeva7@users.noreply.github.com>
2024-04-07 20:14:49 +0400

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -1,13 +1,17 @@
 #import libraries here
 
 def main():
-  herf= input("Enter a letter of the alphabet: ")
+  herf=input("Enter a letter of the alphabet: ")
   if herf=="a" or herf=="e" or herf=="i" or herf=="o" or herf=="u":
       print("Entered alphabet is a consonant!")
   elif herf=="y":
       print("Sometimes it is a vowel, and sometimes it is a consonant!")
   else:
       print("Entered alphabet is a vowel!")
+
+  
+  
+  
   pass
 
 if __name__ == "__main__":
